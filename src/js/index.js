2b0117295caaaeb0f203242e04e564e8f3d0eee7
sbaamoha <sbaamohe@gmail.com>
2022-10-31 20:32:52 +0100

    some changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,13 @@
 let humburger = document.querySelector('#humburger')
 let menu = document.querySelector('.menu')
-
+let titleOne = document.querySelector(".title1")
+let titleTwo = document.querySelector(".title2")
+let titleThree = document.querySelector(".title3")
+let one = document.querySelector(".one")
+let two = document.querySelector(".two")
+let three = document.querySelector(".three")
 humburger.onclick = function (){
     menu.classList.toggle("open")
     humburger.classList.toggle("text-white")
 }
+
