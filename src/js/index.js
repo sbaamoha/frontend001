fb4ee06ac0ebc78acc6d8fd3c207ae94737d0463
sbaamoha <sbaamohe@gmail.com>
2022-11-03 15:18:01 +0100

    completed tabs

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,7 @@
 let humburger = document.querySelector('#humburger')
 let menu = document.querySelector('.menu')
-let titleOne = document.querySelector(".title1")
-let titleTwo = document.querySelector(".title2")
-let titleThree = document.querySelector(".title3")
-let one = document.querySelector(".one")
-let two = document.querySelector(".two")
-let three = document.querySelector(".three")
+let inputBookmarking = document.querySelector('#bookmarking')
 humburger.onclick = function (){
     menu.classList.toggle("open")
     humburger.classList.toggle("text-white")
 }
-
